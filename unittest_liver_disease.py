46ef4e60966ea2c71635e9b36adb8170fbb9185d
Vikas <pawar.vikas@gmail.com>
2024-08-04 11:56:47 +0530

    linting corrections 2

--- unittest_liver_disease.py
+++ unittest_liver_disease.py
@@ -1,20 +1,20 @@
 import unittest
 import os
 import pandas as pd
-from liver_disease import load_data, preprocess_data, impute_missing_values, split_data, train_model, save_model
+from liver_disease import load_data, preprocess_data
+from liver_disease import impute_missing_values
+from liver_disease import split_data, train_model, save_model
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 
 
 class TestLiverDiseaseModel(unittest.TestCase):
 
-
     def test_load_data(self):
         location = "liver_disease_1.csv"
         data = load_data(location)
         self.assertIsInstance(data, pd.DataFrame)
         self.assertGreater(len(data), 0)
-
 
     def test_preprocess_data(self):
         data = load_data("liver_disease_1.csv")
@@ -25,13 +25,11 @@ class TestLiverDiseaseModel(unittest.TestCase):
         self.assertNotIn('Total_Protiens', processed_data.columns)
         self.assertNotIn('Albumin_and_Globulin_Ratio', processed_data.columns)
 
-
     def test_impute_missing_values(self):
         data = load_data("liver_disease_1.csv")
         processed_data = preprocess_data(data)
         imputed_data = impute_missing_values(processed_data)
         self.assertFalse(imputed_data.isnull().values.any())
-
 
     def test_split_data(self):
         data = load_data("liver_disease_1.csv")
@@ -43,7 +41,6 @@ class TestLiverDiseaseModel(unittest.TestCase):
         self.assertGreater(len(Y_train), 0)
         self.assertGreater(len(Y_test), 0)
 
-
     def test_train_model(self):
         data = load_data("liver_disease_1.csv")
         processed_data = preprocess_data(data)
@@ -52,7 +49,6 @@ class TestLiverDiseaseModel(unittest.TestCase):
         model, scaler = train_model(X_train, Y_train)
         self.assertIsInstance(model, RandomForestClassifier)
         self.assertIsInstance(scaler, StandardScaler)
-
 
     def test_save_model(self):
         data = load_data("liver_disease_1.csv")
